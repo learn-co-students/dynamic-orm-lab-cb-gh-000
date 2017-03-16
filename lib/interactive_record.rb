7f647e2b9f676906b264ebe37c59cdc6a2d978e0
Adrián Daraš <getpingui@gmail.com>
2017-03-16 17:02:44 +0100

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,52 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.column_names
+		columns = []
+		DB[:conn].execute("PRAGMA table_info(#{self.table_name})").each do |column|
+			columns << column['name']
+		end
+		columns
+	end
+
+  def self.find_by(hash)
+		DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{hash.keys.first.to_s} = ?", hash.values.first)
+	end
+
+	def self.find_by_name(name)
+		DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name=?", name)
+	end
+
+  def self.table_name
+		self.to_s.downcase.pluralize
+	end
+
+	def initialize(options = {})
+		options.each do |key, value|
+			self.send("#{key}=", value)
+		end
+	end
+
+  def col_names_for_insert
+		self.class.column_names.select{ |name| name != "id" }.join(", ")
+	end
+
+  def save
+		if !self.id
+			DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+			self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+		end
+	end
+
+	def table_name_for_insert
+		self.class.table_name
+	end
+
+	def values_for_insert
+		values = []
+		self.class.column_names.each do |name|
+			values << "'" + self.send("#{name}").to_s + "'" unless name.eql? 'id'
+		end
+		values.join(", ")
+	end
 end
