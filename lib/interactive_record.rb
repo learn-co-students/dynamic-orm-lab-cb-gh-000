10ebba5fa414cba17dcd560ad1af1287a467dc42
robertjosephwayne <robertjwayne@gmail.com>
2017-04-23 21:47:18 -0400

    Add .column_names method to InteractiveRecord class

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,15 +6,22 @@ class InteractiveRecord
     self.attributes = attributes
   end
 
-  def save
-
+  def attributes=(attributes)
   end
 
-  def attributes=(attributes)
+  # Saves instance attributes to database
+  def save
+  end
 
+  # Creates attr_accessors for each column name from database
+  def attr_accessor
+  end
+
+  # Returns an array of SQL column names
+  def self.column_names
+    sql = 'PRAGMA table_info(<table name>)'
   end
 
   def self.find_by_name
-
   end
 end
