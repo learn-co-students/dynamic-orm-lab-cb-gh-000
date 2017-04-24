a2e5f633e6ba314472159885ed7fd4903cf529b3
robertjosephwayne <robertjwayne@gmail.com>
2017-04-23 21:36:16 -0400

    Initialize InteractiveRecord object with hash of attributes

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,19 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def initialize(attributes={})
+    self.attributes = attributes
+  end
+
+  def save
+
+  end
+
+  def attributes=(attributes)
+
+  end
+
+  def self.find_by_name
+
+  end
 end
